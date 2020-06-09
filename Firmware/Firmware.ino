2a425e1091fd52558cee013c6761b7b3b8b10a3a
Silas Waxter <silaswaxter@gmail.com>
2020-06-08 22:23:00 -0700

    Implemented DHT array

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -1,5 +1,6 @@
 #include "hardwareDefinitions.h"
 #include "airFlap.h"
+#include "DHT.h"
 extern "C"
 {
   #include "relay.h"
@@ -14,6 +15,11 @@ waterSwitch_t waterSwitch {WaterSwitch_Pin};
 
 airFlap_t airFlap {AirFlapServo_Pin, AirFlapServo_OpenPosition, AirFlapServo_ClosedPosition};
 
+DHT dht1(DHT1_Pin, 11);
+DHT dht2(DHT2_Pin, 11);
+DHT dht3(DHT3_Pin, 11);
+DHT dht4(DHT4_Pin, 11);
+
 void setup()
 {  
  initRelay(&mister);
@@ -25,14 +31,23 @@ void setup()
  initWaterSwitch(&waterSwitch);
 
  initAirFlap(&airFlap);
+
+ dht1.begin();
+ dht2.begin();
+ dht3.begin();
+ dht4.begin();
  
  Serial.begin(115200);
 }
 
 void loop()
 {
-  enAirFlap(&airFlap);
-  delay(1500);
-  disAirFlap(&airFlap);
-  delay(1500);
+  float h1, h2, h3, h4;
+  h1 = dht1.readHumidity();
+  h2 = dht2.readHumidity();
+  h3 = dht3.readHumidity();
+  h4 = dht4.readHumidity();
+
+  Serial.println(String(h1) + ", " + String(h2) + ", " +  String(h3) + ", " +  String(h4));
+  delay(1000);
 }
